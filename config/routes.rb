5c8c25d4fbeb9a1fc8c02ac8d01739a237d903f6
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-26 16:17:52 -0500

    Crea la ruta /products/:id/edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/products/new' , to: 'products#new', as: 'new_product'
   #almacenar producto base de datos
   post '/products' , to: 'products#create'
-  #ruta para mostrar detalles de un id 
-  get '/products/:id' ,to: 'products#show'
+  #ruta para mostrar detalles de un id
+  get '/products/:id' ,to: 'products#show' , as: 'product'
+  #ruta editar product
+  get '/products/:id/edit' ,to: 'products#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
